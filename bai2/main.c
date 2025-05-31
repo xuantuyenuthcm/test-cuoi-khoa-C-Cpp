d834929aa7972b7b014fe4a7bb4254230c8c0de5
xuantuyenuthcm <nxuantuyen2204@gmail.com>
2025-05-31 12:32:38 +0700

    Bai_2
    push bai 2

---
+++ bai2/main.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+void print_high_low_bytes(unsigned short value) {
+    unsigned char high = (value >> 8) & 0xFF;  // 8 bit cao
+    unsigned char low = value & 0xFF;          // 8 bit thấp
+
+    printf("High byte: %u (0x%02X)\n", high, high);
+    printf("Low byte: %u (0x%02X)\n", low, low);
+}
+
+int main() {
+    unsigned short number = 1234; 
+    print_high_low_bytes(number);
+    return 0;
+}
